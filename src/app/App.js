23c22760419ffa7f8d2fd17246fa669e4a1fd07d
Oui-Dev <kevin.blaise24@gmail.com>
2022-06-15 13:44:37 +0200

    feat: ajout react + react router + husky + prettier + tailwind + sass

---
+++ src/app/App.js
@@ -0,0 +1,16 @@
+import './App.scss'
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Home from './home/Home'
+
+export default function App() {
+    return (
+        <Router>
+            <header className="App-header">
+                <p>React</p>
+            </header>
+            <Routes>
+                <Route path="/" element={<Home />} />
+            </Routes>
+        </Router>
+    )
+}
