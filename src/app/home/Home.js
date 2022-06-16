7f8f5032c900be6f7e9a00ded6716bad2310296d
Oui-Dev <kevin.blaise24@gmail.com>
2022-06-16 13:49:36 +0200

    feat: récupération et stockage de l'accessToken

--- src/app/home/Home.js
+++ src/app/home/Home.js
@@ -5,18 +5,77 @@ import CardContent from '@mui/material/CardContent'
 import CardMedia from '@mui/material/CardMedia'
 import Typography from '@mui/material/Typography'
 import {CardActionArea} from '@mui/material'
+
 import imgTest from '../../assets/contemplative-reptile.jpg'
 
+import axios from 'axios'
+import {Buffer} from 'buffer'
+import {useState, useEffect} from 'react'
+
 export default function Home() {
+    const [accessToken, setAccessToken] = useState()
+
     const itemsArray = [
-        {title: 'pute1', desc: 'je suis la pute1'},
-        {title: 'pute2', desc: 'je suis la pute2'},
-        {title: 'pute3', desc: 'je suis la pute3'},
-        {title: 'pute4', desc: 'je suis la pute4'},
-        {title: 'pute5', desc: 'je suis la pute5'},
-        {title: 'pute6', desc: 'je suis la pute6'},
-        {title: 'pute7', desc: 'je suis la pute7'},
+        {title: 'test1', desc: 'je suis le test1'},
+        {title: 'test2', desc: 'je suis le test2'},
+        {title: 'test3', desc: 'je suis le test3'},
+        {title: 'test4', desc: 'je suis le test4'},
+        {title: 'test5', desc: 'je suis le test5'},
+        {title: 'test6', desc: 'je suis le test6'},
+        {title: 'test7', desc: 'je suis le test7'},
     ]
+
+    useEffect(() => {
+        if (!accessToken) {
+            if (document.cookie.split(';').some((item) => item.trim().startsWith('token='))) {
+                const cookieValue = document.cookie.split('; ').find((row) => row.startsWith('token='))
+                const data = JSON.parse(Buffer.from(cookieValue.split('=')[1], 'base64').toString())
+                setAccessToken(data.access_token)
+            } else {
+                getNewAccessToken()
+            }
+        }
+    }, [])
+
+    function getNewAccessToken() {
+        var data = new FormData()
+        data.append('client_id', 'ce9ecff3e16a7b6')
+        data.append('client_secret', 'dc7a9ac237093e0986ae231bb1e00ccc3c45c4bb')
+        data.append('refresh_token', 'b22d91c293b4cde3a93f3b71e87a38e77eeb8757')
+        data.append('grant_type', 'refresh_token')
+
+        const config = {
+            method: 'POST',
+            url: 'https://api.imgur.com/oauth2/token',
+            data: data,
+        }
+
+        axios(config)
+            .then(function (response) {
+                const data = Buffer.from(JSON.stringify(response.data)).toString('base64')
+                const delay = response.data.expires_in - 60
+                document.cookie = `token=${data}; path=/; max-age=${delay};`
+                setAccessToken(response.data.access_token)
+            })
+            .catch(function (error) {
+                console.log(error)
+            })
+    }
+
+    // recup les images du compte
+    var config = {
+        method: 'get',
+        url: 'https://api.imgur.com/3/account/me/images',
+        headers: {Authorization: `Bearer ${accessToken}`},
+    }
+
+    axios(config)
+        .then(function (response) {
+            console.log(JSON.stringify(response.data))
+        })
+        .catch(function (error) {
+            console.log(error)
+        })
 
     const CardItem = ({name, desc}) => {
         return (
