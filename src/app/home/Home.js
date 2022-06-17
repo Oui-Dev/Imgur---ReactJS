b8984ad860fd30bae10270aa5f7f5ae7d72a8aeb
Oui-Dev <kevin.blaise24@gmail.com>
2022-06-17 13:58:44 +0200

    feat: display viral img + change display possibility

--- src/app/home/Home.js
+++ src/app/home/Home.js
@@ -1,32 +1,31 @@
 import './Home.scss'
-import Grid from '@mui/material/Grid'
-import Card from '@mui/material/Card'
-import CardContent from '@mui/material/CardContent'
-import CardMedia from '@mui/material/CardMedia'
-import Typography from '@mui/material/Typography'
-import {CardActionArea} from '@mui/material'
+import {Grid, Card, CardContent, CardMedia, CardActions, Typography, Stack, Switch} from '@mui/material'
+import {QueryClient, QueryClientProvider, useQuery} from 'react-query'
+import {useState, useEffect} from 'react'
+import {Buffer} from 'buffer'
+import axios from 'axios'
 
 import imgTest from '../../assets/contemplative-reptile.jpg'
 
-import {QueryClient, QueryClientProvider, useQuery} from 'react-query'
-import axios from 'axios'
-import {Buffer} from 'buffer'
-import {useState, useEffect} from 'react'
+const initialConfig = {
+    method: 'get',
+    url: 'https://api.imgur.com/3/gallery/hot/viral?showViral=true&mature=true',
+    headers: {Authorization: 'Client-ID ce9ecff3e16a7b6'},
+}
 
 export default function Home() {
     const [accessToken, setAccessToken] = useState()
+    const [displayConfig, setDisplayConfig] = useState(initialConfig)
     const queryClient = new QueryClient()
 
     // recup le token d'accès
     useEffect(() => {
-        if (!accessToken) {
-            if (document.cookie.split(';').some((item) => item.trim().startsWith('token='))) {
-                const cookieValue = document.cookie.split('; ').find((row) => row.startsWith('token='))
-                const data = JSON.parse(Buffer.from(cookieValue.split('=')[1], 'base64').toString())
-                setAccessToken(data.access_token)
-            } else {
-                getNewAccessToken()
-            }
+        if (document.cookie.split(';').some((item) => item.trim().startsWith('token='))) {
+            const cookieValue = document.cookie.split('; ').find((row) => row.startsWith('token='))
+            const data = JSON.parse(Buffer.from(cookieValue.split('=')[1], 'base64').toString())
+            setAccessToken(data.access_token)
+        } else {
+            getNewAccessToken()
         }
     }, [])
 
@@ -56,15 +55,9 @@ export default function Home() {
             })
     }
 
-    // recup les images du compte
-    function GetProfileGallery({accessToken}) {
-        const config = {
-            method: 'get',
-            url: 'https://api.imgur.com/3/account/me/images',
-            headers: {Authorization: `Bearer ${accessToken}`},
-        }
-
-        const {isLoading, error, data, isFetching} = useQuery('repoData', () => axios(config).then((res) => res.data))
+    // recup les images selon la config
+    function GetGallery({config}) {
+        const {isLoading, error, data, isFetching} = useQuery('getGallery', () => axios(config).then((res) => res.data))
 
         if (isLoading)
             return (
@@ -85,7 +78,7 @@ export default function Home() {
             <Grid container spacing={3} columns={{xs: 3, sm: 6, md: 9, lg: 12}}>
                 {Array.from(data.data).map((arrItem, index) => (
                     <Grid item xs={3} key={index}>
-                        <CardItem name={arrItem.title} desc={arrItem.desc} />
+                        <CardItem data={arrItem} />
                     </Grid>
                 ))}
                 {data.data.length === 0 ? (
@@ -102,31 +95,66 @@ export default function Home() {
         )
     }
 
-    const CardItem = ({name, desc}) => {
+    // change la config axios pour modifier l'affichage
+    function changeDisplay(state) {
+        let config = {}
+        if (state === false) {
+            config = initialConfig
+        } else {
+            config = {
+                method: 'get',
+                url: 'https://api.imgur.com/3/account/me/images',
+                headers: {Authorization: `Bearer ${accessToken}`},
+            }
+        }
+        setDisplayConfig(config)
+    }
+
+    const CardItem = ({data}) => {
+        console.log(data.images)
         return (
             <Card sx={{maxWidth: 345}}>
-                <CardActionArea>
-                    <CardMedia component="img" height="140" image={imgTest} alt="green iguana" />
-                    <CardContent>
-                        <Typography gutterBottom variant="h5" component="div">
-                            {name}
-                        </Typography>
-                        <Typography variant="body2" color="text.secondary">
-                            {desc}
-                        </Typography>
-                    </CardContent>
-                </CardActionArea>
+                <CardMedia component="img" height="140" image={imgTest} alt="green iguana" />
+                <CardContent>
+                    <Typography gutterBottom variant="h7" component="div">
+                        {data.title}
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                        {data.description}
+                    </Typography>
+                </CardContent>
+                <CardActions>
+                    <div className="cardActionsFooter">
+                        {data.ups}
+                        <i className="bx bxs-upvote"></i>
+                    </div>
+                    <div className="cardActionsFooter">
+                        {data.comment_count}
+                        <i className="bx bxs-chat"></i>
+                    </div>
+                    <div className="cardActionsFooter">
+                        {data.views}
+                        <i className="bx bxs-show"></i>
+                    </div>
+                </CardActions>
             </Card>
         )
     }
 
     return (
         <div className="homeContent">
-            {accessToken ? (
-                <QueryClientProvider client={queryClient}>
-                    <GetProfileGallery accessToken={accessToken} />
-                </QueryClientProvider>
-            ) : null}
+            <Stack direction="row" spacing={1} alignItems="center" className="switch">
+                <Typography>Viral</Typography>
+                <Switch
+                    onClick={(e) => {
+                        changeDisplay(e.target.checked)
+                    }}
+                />
+                <Typography>Personnel</Typography>
+            </Stack>
+            <QueryClientProvider client={queryClient}>
+                <GetGallery config={displayConfig} />
+            </QueryClientProvider>
             <i
                 className="bx bx-cloud-upload"
                 onClick={(e) => {
